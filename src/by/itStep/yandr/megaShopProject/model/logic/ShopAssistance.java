940bb9295381f67d0ac2ae89062094139f0e3ffa
Yandr2022 <sharkievich@gmail.com>
2022-08-19 17:05:54 +0300

    Added  ShopAssistance class prototype

---
+++ src/by/itStep/yandr/megaShopProject/model/logic/ShopAssistance.java
@@ -0,0 +1,14 @@
+package by.itStep.yandr.megaShopProject.model.logic;
+
+import by.itStep.yandr.megaShopProject.model.entity.container.Basket;
+
+public class ShopAssistance {
+
+    private ShopAssistance() {}
+
+    public static double calculateTotalPrice(Basket basket) {
+        double total = 0;
+        return total;
+    }
+}
+
