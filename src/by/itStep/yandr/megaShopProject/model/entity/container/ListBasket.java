e8849f30889e0d648b419f58bb3b2cd95c36eaf1
Yandr2022 <sharkievich@gmail.com>
2022-09-10 11:07:46 +0300

    Added dynamic container implementation based on singly linked list

--- src/by/itStep/yandr/megaShopProject/model/entity/container/ListBasket.java
+++ src/by/itStep/yandr/megaShopProject/model/entity/container/ListBasket.java
@@ -1,4 +1,62 @@
 package by.itStep.yandr.megaShopProject.model.entity.container;
 
+import by.itStep.yandr.megaShopProject.model.entity.abstracts.Product;
+
 public class ListBasket {
+    private Node first;
+    private int size;
+
+    public ListBasket() {
+        first = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void add(Product product) {
+        if (isEmpty()) {
+            first = new Node(product);
+        } else {
+            Node temp = first;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = new Node(product);
+        }
+        size++;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+
+        if (isEmpty()) {
+            builder.append("Basket is empty.");
+        } else {
+            builder.append("Basket:");
+            Node temp = first;
+            while (temp.next != null) {
+                builder.append("\n").append(temp.product);
+                temp = temp.next;
+            }
+            builder.append(temp.product);
+        }
+
+        return builder.toString();
+    }
+
+    private class Node {
+        public Product product;
+        public Node next;
+
+        public Node(Product product) {
+            this.product = product;
+        }
+    }
 }
